b8580c39bc42763a6e0d1aa42247b67804eba8af
Santiago Rios <santirios32@gmail.com>
2024-05-22 00:14:13 -0300

    Hover y scale

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -4,11 +4,11 @@ import CartWidget from '../cartwidget/cartwidget'
 const NavBar = () => {
   return (
     <nav className='flex justify-between items-center w-full py-3 px-10 font-ubuntu text-white'>
-       <h2 className='flex flex-grow basis-0 text-lg'>Mar Indumentaria</h2>
+       <h2 className='flex flex-grow basis-0 text-lg'> <a className='hover:scale-105 hover:text-gray-300' href="">Mar Indumentaria</a></h2>
         <ul className='flex flex-grow basis-0 gap-3 text-lg justify-center'>
-            <li><a className='hover:text-slate-500' href="">Inicio</a></li>
-            <li><a className='hover:text-slate-500' href="">Productos</a></li>
-            <li><a className='hover:text-slate-500' href="">Contacto</a></li>
+            <li><a className='hover:text-gray-300' href="">Inicio</a></li>
+            <li><a className='hover:text-gray-300' href="">Productos</a></li>
+            <li><a className='hover:text-gray-300' href="">Contacto</a></li>
         </ul>
         <CartWidget></CartWidget>
     </nav>
